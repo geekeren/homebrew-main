9f8ef3a51567e9d8e425f54652b574d20d23ef1b
Baiyuan Wang <bywang@thoughtworks.com>
2018-12-28 09:19:57 +0800

    Update twvpn.rb

--- Formula/twvpn.rb
+++ Formula/twvpn.rb
@@ -1,14 +1,14 @@
 class Twvpn < Formula
   desc "Tool to connect to TWVPN at one click"
   homepage "https://twvpn.wangbaiyuan.cn"
-  url "https://github.com/geekeren/twvpn/archive/v0.2.tar.gz"
-  sha256 "daf185331faa22e25ac1c42f2903c34535ea7b9fc2d9ce2a02a6e71e3b9a6f81"
+  url "https://github.com/geekeren/twvpn/archive/v0.3.tar.gz"
+  sha256 "e3f05cac6274f61f00a56f9d74c12ae7472fb8262bd523b8f9b4154d844cc763"
   depends_on "oath-toolkit"
   def install
     bin.install "twvpn"
   end
 
   test do
-    system "#{bin}/twvpn", "--version"
+    system "#{bin}/twvpn", "-version"
   end
 end
